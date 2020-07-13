1ec63a6dd18c58262793e7e7e8ffc3f4a5b4616c
RichardLiao <bnmghjtyu0@gmail.com>
2020-07-13 22:00:28 +0800

    使用 svg

--- src/App.js
+++ src/App.js
@@ -8,6 +8,30 @@ import {createStackNavigator} from '@react-navigation/stack';
 import CategoryScreen from './pages/Category';
 import MovieScreen from './pages/MovieScreen';
 import EnglishCard from './pages/EnglishCard';
+import {SvgCss} from 'react-native-svg';
+
+const xml = (obj) =>
+  `<svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="${obj.color}"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="feather feather-film">
+    <rect x="2" y="2" width="20" height="20" rx="2.18" ry="2.18"></rect>
+    <line x1="7" y1="2" x2="7" y2="22"></line>
+    <line x1="17" y1="2" x2="17" y2="22"></line>
+    <line x1="2" y1="12" x2="22" y2="12"></line>
+    <line x1="2" y1="7" x2="7" y2="7"></line>
+    <line x1="2" y1="17" x2="7" y2="17"></line>
+    <line x1="17" y1="17" x2="22" y2="17"></line>
+    <line x1="17" y1="7" x2="22" y2="7"></line>
+  </svg>
+`;
 
 const AppStack = createStackNavigator();
 
@@ -59,7 +83,13 @@ const App = () => {
               if (route.name === 'Search') {
                 iconName = focused ? 'ios-search' : 'ios-search';
               } else if (route.name === 'Movie') {
-                iconName = focused ? 'ios-list-box' : 'ios-list';
+                return (
+                  <SvgCss
+                    xml={xml({
+                      color: `${(iconName = focused ? 'tomato' : 'gray')}`,
+                    })}
+                  />
+                );
               } else if (route.name === 'Category') {
                 iconName = focused ? 'ios-albums' : 'ios-albums';
               }
